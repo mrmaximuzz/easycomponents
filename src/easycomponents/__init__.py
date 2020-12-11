b262df732b86f64b13544cd22726858ebd0a7192
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-12-11 21:55:22 +0300

    Add empty files

---
+++ src/easycomponents/__init__.py
@@ -0,0 +1,5 @@
+"""Helper utility for software architects to manage components in a project
+
+Takes a simple YAML description of your components and tries to find weaknesses
+in the project architecture. It also generates .dot description which you can
+use for project structure visualization."""
---
+++ src/easycomponents/component.py
@@ -0,0 +1,3 @@
+"""
+Main entities for components analyzer
+"""
