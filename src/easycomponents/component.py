4a35bc3ad976507f92d85743cd9c5e6db2bf1368
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-12-11 22:18:24 +0300

    Add simple component definition

--- src/easycomponents/component.py
+++ src/easycomponents/component.py
@@ -1,3 +1,16 @@
 """
-Main entities for components analyzer
+Software component description
 """
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class SimpleComponent:
+    """The simplest component description: name and dependencies"""
+
+    name: str
+    deps: List[SimpleComponent]
