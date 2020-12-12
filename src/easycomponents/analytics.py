b6cb1d1bb3323a0957b73c3415f9993cf851fff3
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-12-12 03:25:26 +0300

    Typo

--- src/easycomponents/analytics.py
+++ src/easycomponents/analytics.py
@@ -6,6 +6,6 @@ from .component import SimpleComponent
 
 
 def check_duplicates(components: List[SimpleComponent]):
-    """Check a list given for a components with same names"""
+    """Check a list given for components with same names"""
     names = {comp.name for comp in components}
     return len(names) == len(components)
