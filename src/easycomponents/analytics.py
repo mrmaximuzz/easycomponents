078b707541cc1eb8354d723c7a9856eaa4da95b4
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-12-13 22:10:43 +0300

    Improved duplicates search

--- src/easycomponents/analytics.py
+++ src/easycomponents/analytics.py
@@ -5,7 +5,13 @@ from typing import List
 from .component import SimpleComponent
 
 
-def check_duplicates(components: List[SimpleComponent]):
+def check_duplicates(components: List[SimpleComponent]) -> List[str]:
     """Check a list given for components with same names"""
-    names = {comp.name for comp in components}
-    return len(names) == len(components)
+    duplicates = set()
+
+    names = sorted(comp.name for comp in components)
+    for name, other in zip(names[:-1], names[1:]):
+        if name == other:
+            duplicates.add(name)
+
+    return sorted(duplicates)
--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -6,12 +6,17 @@ from easycomponents.component import SimpleComponent
 
 def test_check_duplicates():
     """Test searching for duplicate names"""
-    assert check_duplicates([])  # extreme case
+    assert check_duplicates([]) == []  # extreme case
 
     comp1 = SimpleComponent("A", [])
     comp2 = SimpleComponent("B", [])
-    assert check_duplicates([comp1, comp2])
+    assert check_duplicates([comp1, comp2]) == []
 
     comp1 = SimpleComponent("A", [])
     comp2 = SimpleComponent("A", [])
-    assert not check_duplicates([comp1, comp2])
+    assert check_duplicates([comp1, comp2]) == ["A"]
+
+    comp1 = SimpleComponent("A", [])
+    comp2 = SimpleComponent("B", [])
+    comp3 = SimpleComponent("A", [])
+    assert check_duplicates([comp1, comp2, comp3]) == ["A"]
