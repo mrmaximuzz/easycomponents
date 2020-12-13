a4657cf2390131aca44202c7c25bdf2facb4f633
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-12-13 23:18:11 +0300

    Add test for incorrect dependencies

--- src/easycomponents/analytics.py
+++ src/easycomponents/analytics.py
@@ -1,12 +1,35 @@
 """Module for various components analysis"""
 
 import itertools
+from dataclasses import dataclass
 from typing import List
 
 from .component import SimpleComponent
+
+
+@dataclass
+class DepError:
+    """Error in dependency"""
+
+    component: str
+    dependency: str
 
 
 def find_duplicates(components: List[SimpleComponent]) -> List[str]:
     """Check a list given for components with same names"""
     names = sorted(comp.name for comp in components)
     return [k for k, g in itertools.groupby(names) if len(list(g)) > 1]
+
+
+def find_incorrect_deps(components: List[SimpleComponent]) -> List[DepError]:
+    """Find inexistent dependencies for the components list"""
+    names = {comp.name for comp in components}
+
+    errs = []
+    for comp in components:
+        for dep in comp.deps:
+            if dep not in names:
+                err = DepError(comp.name, dep)
+                errs.append(err)
+
+    return errs
--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -1,10 +1,13 @@
 """Module for testing analytic implementation"""
 
 from easycomponents.analytics import find_duplicates
+from easycomponents.analytics import find_incorrect_deps
+from easycomponents.analytics import DepError
+
 from easycomponents.component import SimpleComponent
 
 
-def test_check_duplicates():
+def test_find_duplicates():
     """Test searching for duplicate names"""
     assert not find_duplicates([])
 
@@ -20,3 +23,34 @@ def test_check_duplicates():
     comp2 = SimpleComponent("B", [])
     comp3 = SimpleComponent("A", [])
     assert find_duplicates([comp1, comp2, comp3]) == ["A"]
+
+
+def test_find_incorrect_deps():
+    """Test for searching error dependencies"""
+
+    comp1 = SimpleComponent("A", [])
+    comp2 = SimpleComponent("B", [])
+    assert not find_incorrect_deps([comp1, comp2])
+
+    comp1 = SimpleComponent("A", ["B"])
+    comp2 = SimpleComponent("B", [])
+    assert not find_incorrect_deps([comp1, comp2])
+
+    comp1 = SimpleComponent("A", [])
+    comp2 = SimpleComponent("B", ["A"])
+    assert not find_incorrect_deps([comp1, comp2])
+
+    comp1 = SimpleComponent("A", ["X"])
+    comp2 = SimpleComponent("B", [])
+    errs = [DepError("A", "X")]
+    assert find_incorrect_deps([comp1, comp2]) == errs
+
+    comp1 = SimpleComponent("A", ["X", "Y"])
+    comp2 = SimpleComponent("B", [])
+    errs = [DepError("A", "X"), DepError("A", "Y")]
+    assert find_incorrect_deps([comp1, comp2]) == errs
+
+    comp1 = SimpleComponent("A", ["X", "Y"])
+    comp2 = SimpleComponent("B", ["Z"])
+    errs = [DepError("A", "X"), DepError("A", "Y"), DepError("B", "Z")]
+    assert find_incorrect_deps([comp1, comp2]) == errs
