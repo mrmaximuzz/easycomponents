edc061fc3e1b6edede451423aae4258ae3e9f926
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-12-13 22:39:27 +0300

    Simplify component structure

--- src/easycomponents/component.py
+++ src/easycomponents/component.py
@@ -2,15 +2,17 @@
 Software component description
 """
 
-from __future__ import annotations
-
 from dataclasses import dataclass
 from typing import List
 
 
 @dataclass
 class SimpleComponent:
-    """The simplest component description: name and dependencies"""
+    """The simplest component description: name and dependencies
+
+    name: unique component's identifier, acts as a key in storages
+    deps: list of dependencies referenced by name
+    """
 
     name: str
-    deps: List[SimpleComponent]
+    deps: List[str]
