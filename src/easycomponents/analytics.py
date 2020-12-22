5a3761037e0c1c9da0ebb2b23ba95af64bd5c1cd
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-12-22 22:27:05 +0300

    Use Collection instead of List

--- src/easycomponents/analytics.py
+++ src/easycomponents/analytics.py
@@ -2,7 +2,7 @@
 
 import itertools
 from dataclasses import dataclass
-from typing import List
+from typing import Collection, List
 
 from .component import SimpleComponent
 
@@ -15,14 +15,14 @@ class DepError:
     dependency: str
 
 
-def find_duplicates(components: List[SimpleComponent]) -> List[str]:
-    """Check a list given for components with same names"""
+def find_duplicates(components: Collection[SimpleComponent]) -> List[str]:
+    """Check a given collection for components with same names"""
     names = sorted(comp.name for comp in components)
     return [k for k, g in itertools.groupby(names) if len(list(g)) > 1]
 
 
-def find_incorrect_deps(components: List[SimpleComponent]) -> List[DepError]:
-    """Find inexistent dependencies for the components list"""
+def find_incorrect_deps(components: Collection[SimpleComponent]) -> List[DepError]:
+    """Find inexistent dependencies for the components collection"""
     names = {comp.name for comp in components}
 
     errs = []
