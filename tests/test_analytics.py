ecaf71211cf93f27958c4f082a71661abdae1437
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-12-23 22:44:30 +0300

    Reorganize tests

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -11,53 +11,77 @@ def test_find_dups():
     """Test searching for duplicate names"""
     assert not find_dups([])
 
-    comp1 = SimpleComponent("A", [])
-    comp2 = SimpleComponent("B", [])
-    assert not find_dups([comp1, comp2])
+    comps = [
+        SimpleComponent("A", []),
+        SimpleComponent("B", []),
+    ]
+    assert not find_dups(comps)
 
-    comp1 = SimpleComponent("A", [])
-    comp2 = SimpleComponent("A", [])
-    assert find_dups([comp1, comp2]) == [Duplicate("A", 2)]
+    comps = [
+        SimpleComponent("A", []),
+        SimpleComponent("A", []),
+    ]
+    dups = [Duplicate("A", 2)]
+    assert find_dups(comps) == dups
 
-    comp1 = SimpleComponent("A", [])
-    comp2 = SimpleComponent("A", [])
-    comp3 = SimpleComponent("A", [])
-    assert find_dups([comp1, comp2, comp3]) == [Duplicate("A", 3)]
+    comps = [
+        SimpleComponent("A", []),
+        SimpleComponent("A", []),
+        SimpleComponent("A", []),
+    ]
+    dups = [Duplicate("A", 3)]
+    assert find_dups(comps) == dups
 
-    comp1 = SimpleComponent("A", [])
-    comp2 = SimpleComponent("B", [])
-    comp3 = SimpleComponent("A", [])
-    comp4 = SimpleComponent("B", [])
+    comps = [
+        SimpleComponent("A", []),
+        SimpleComponent("B", []),
+        SimpleComponent("A", []),
+        SimpleComponent("B", []),
+    ]
     errs = [Duplicate("A", 2), Duplicate("B", 2)]
-    assert find_dups([comp1, comp2, comp3, comp4]) == errs
+    assert find_dups(comps) == errs
 
 
 def test_find_dangling():
     """Test for searching error dependencies"""
 
-    comp1 = SimpleComponent("A", [])
-    comp2 = SimpleComponent("B", [])
-    assert not find_dangling([comp1, comp2])
+    assert not find_dangling([])
 
-    comp1 = SimpleComponent("A", ["B"])
-    comp2 = SimpleComponent("B", [])
-    assert not find_dangling([comp1, comp2])
+    comps = [
+        SimpleComponent("A", []),
+        SimpleComponent("B", []),
+    ]
+    assert not find_dangling(comps)
 
-    comp1 = SimpleComponent("A", [])
-    comp2 = SimpleComponent("B", ["A"])
-    assert not find_dangling([comp1, comp2])
+    comps = [
+        SimpleComponent("A", ["B"]),
+        SimpleComponent("B", []),
+    ]
+    assert not find_dangling(comps)
 
-    comp1 = SimpleComponent("A", ["X"])
-    comp2 = SimpleComponent("B", [])
+    comps = [
+        SimpleComponent("A", []),
+        SimpleComponent("B", ["A"]),
+    ]
+    assert not find_dangling(comps)
+
+    comps = [
+        SimpleComponent("A", ["X"]),
+        SimpleComponent("B", []),
+    ]
     errs = [Dangling("A", "X")]
-    assert find_dangling([comp1, comp2]) == errs
+    assert find_dangling(comps) == errs
 
-    comp1 = SimpleComponent("A", ["X", "Y"])
-    comp2 = SimpleComponent("B", [])
+    comps = [
+        SimpleComponent("A", ["X", "Y"]),
+        SimpleComponent("B", []),
+    ]
     errs = [Dangling("A", "X"), Dangling("A", "Y")]
-    assert find_dangling([comp1, comp2]) == errs
+    assert find_dangling(comps) == errs
 
-    comp1 = SimpleComponent("A", ["X", "Y"])
-    comp2 = SimpleComponent("B", ["Z"])
+    comps = [
+        SimpleComponent("A", ["X", "Y"]),
+        SimpleComponent("B", ["Z"]),
+    ]
     errs = [Dangling("A", "X"), Dangling("A", "Y"), Dangling("B", "Z")]
-    assert find_dangling([comp1, comp2]) == errs
+    assert find_dangling(comps) == errs
